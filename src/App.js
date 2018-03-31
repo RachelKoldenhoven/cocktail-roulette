16cfbbc9ac614edcfcf2218ac74f462a8a0ce988
RachelKoldenhoven <rachel@koldenfrozen.com>
2018-03-30 22:02:16 -0600

    add button for new drink

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
                 for (let i = 1; i < 15; i++) {
                     let ingredientKey = "strIngredient" + i;
                     let quantityKey = "strMeasure" + i;
+                    if(drink[ingredientKey] === null) return;
                     if (drink[ingredientKey].length > 0) {
                         ingredients.push({ingredient: drink[ingredientKey], quantity: drink[quantityKey]});
                     }
@@ -48,21 +49,54 @@ class App extends Component {
             })
     }
 
+    showMeAnother() {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                const drink = data.drinks[0];
+                console.log(drink);
+                let ingredients = [];
+                for (let i = 1; i < 15; i++) {
+                    let ingredientKey = "strIngredient" + i;
+                    let quantityKey = "strMeasure" + i;
+                    if(drink[ingredientKey] === null) return;
+                    if (drink[ingredientKey].length > 0) {
+                        ingredients.push({
+                            ingredient: drink[ingredientKey],
+                            quantity: drink[quantityKey]
+                        });
+                    }
+                }
+                this.setState({
+                    drink: {
+                        name: drink.strDrink,
+                        ingredients: ingredients,
+                        pic: drink.strDrinkThumb,
+                        instructions: drink.strInstructions
+                    }
+                });
+
+            })    }
+
     render() {
         const drink = this.state.drink;
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={require("./assets/david-straight-341873-unsplash.jpg")} className="App-logo" alt="logo"/>
+                    <img src={require("./assets/david-straight-341873-unsplash.jpg")} className="App-logo" alt="David Straight on Unsplash"/>
                 </header>
                 <section className="drinkBox">
                     <h1>{drink.name}</h1>
                     <img className="drinkPic" alt={drink.name} src={drink.pic}/>
                     <ul className="ingredientList">
                         {drink.ingredients.map((ingredient) =>
-                            <li key={ingredient.ingredient}>{ingredient.ingredient}: {ingredient.quantity}</li>)}
+                            <li key={ingredient.ingredient}>
+                                <span>{ingredient.ingredient}</span>
+                                <span>{(ingredient.quantity) ? " : " + ingredient.quantity : null }</span>
+                            </li>)}
                     </ul>
                     <p className="instructions">{drink.instructions}</p>
+                    <button onClick={() => this.showMeAnother()}>Show Me Another</button>
                 </section>
             </div>
         );
